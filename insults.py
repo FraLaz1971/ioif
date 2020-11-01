98758bd04ee7a02570e956230fed3ad1150a8d80
Francesco Lazzarotto <fralaz1971@gmail.com>
2020-11-01 11:20:21 +0100

    updated python's

--- insults.py
+++ insults.py
@@ -1,9 +1,9 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 print("this python program make insults to people who deserve")
-man = raw_input("what is his name? ")
-woman = raw_input("what is her name?")
+man = input("what is his name? ")
+woman = input("what is her name?")
 if  ( man == "Alessandro" ) and ( woman == "Anna" ):
     print("Fuck you "+man+" and "+woman+"! Pieces of shit!")
 else:
     print("Hey, glad to meet you. "+man+" and "+woman+"!")
-print "bye"
+print("bye")
