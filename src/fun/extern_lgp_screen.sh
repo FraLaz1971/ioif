333df4a7bb3bb7f37fa985214b42278e4dcd996c
Francesco Lazzarotto <fralaz1971@gmail.com>
2020-11-13 14:44:54 +0100

    after running with external bash launcher

---
+++ src/fun/extern_lgp_screen.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+echo "executing extern gnuplot launch $0 $@"
+echo "extern_lgp_screen.sh: creating temp. files"
+./testlibfun table.csv 1 1
+ls *.gp *tsv
+echo "executing extern gnuplot launch $0 $@"
+
+gnuplot << EOF
+set terminal qt persist
+set xlabel 'time (s)'
+set ylabel 'counts #'
+set title 'My Awesome Plot'
+plot 'temp.tsv' using 1:2
+EOF
